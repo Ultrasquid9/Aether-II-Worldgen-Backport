07d46c7f0b951d316e15174d16f60131a9541669
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-09-23 19:25:51 -0400

    Remove Config Code

--- src/main/java/juni/aether_ii_worldgen_backport/AetherIIWorldgenBackport.java
+++ src/main/java/juni/aether_ii_worldgen_backport/AetherIIWorldgenBackport.java
@@ -4,19 +4,15 @@ import com.mojang.logging.LogUtils;
 
 import juni.aether_ii_worldgen_backport.world.feature.AetherIIWorldgenBackportFeatures;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -32,9 +28,6 @@ public class AetherIIWorldgenBackport
     {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
         DeferredRegister<?>[] registers = {
 			AetherIIWorldgenBackportFeatures.FEATURES
 		};
@@ -45,22 +38,6 @@ public class AetherIIWorldgenBackport
         for (DeferredRegister<?> register : registers) {
             register.register(modEventBus);
         }
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
