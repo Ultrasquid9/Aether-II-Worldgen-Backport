320c3a1fa7053ba2691eddd66f5dcd7090d9e599
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-09-23 20:55:31 -0400

    Update AetherIIWorldgenBackport.java

--- src/main/java/juni/aether_ii_worldgen_backport/AetherIIWorldgenBackport.java
+++ src/main/java/juni/aether_ii_worldgen_backport/AetherIIWorldgenBackport.java
@@ -1,19 +1,10 @@
 package juni.aether_ii_worldgen_backport;
 
-import com.mojang.logging.LogUtils;
-
 import juni.aether_ii_worldgen_backport.world.feature.AetherIIWorldgenBackportFeatures;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(AetherIIWorldgenBackport.MODID)
@@ -21,43 +12,15 @@ public class AetherIIWorldgenBackport
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "aether_ii_worldgen_backport";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
-    public AetherIIWorldgenBackport(FMLJavaModLoadingContext context)
+    public AetherIIWorldgenBackport()
     {
-        IEventBus modEventBus = context.getModEventBus();
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        DeferredRegister<?>[] registers = {
-			AetherIIWorldgenBackportFeatures.FEATURES
-		};
+		// Registerring
+		AetherIIWorldgenBackportFeatures.FEATURES.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        for (DeferredRegister<?> register : registers) {
-            register.register(modEventBus);
-        }
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
     }
 }
